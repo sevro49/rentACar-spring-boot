e93a47801aa840b3a3b02a296282a96ada9ba0cb
sevro49 <95761902+sevro49@users.noreply.github.com>
2024-07-19 03:13:32 +0300

    added swagger ui

--- src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
+++ src/main/java/kodlama/io/rentACar/webApi/controllers/BrandsController.java
@@ -19,7 +19,7 @@ public class BrandsController {
         this.brandService = brandService;
     }
 
-    @GetMapping("/getAll")
+    @GetMapping("/getall")
     public List<Brand> getAll(){
         return brandService.getAll();
     }
